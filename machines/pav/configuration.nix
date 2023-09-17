2d6de0d2c3884cc37a97276547a03169e9fa44bb
Kris Williams <115474+kriswill@users.noreply.github.com>
2023-09-16 21:17:32 -0700

    Fix build for pav

--- machines/pav/configuration.nix
+++ machines/pav/configuration.nix
@@ -28,7 +28,7 @@
     # You can add overlays here
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
+      # outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
 
@@ -48,7 +48,7 @@
         #nordvpn
         nordvpn = config.nur.repos.LuisChDev.nordvpn;
       };
-      
+
       # Disable if you don't want unfree packages
       allowUnfree = true;
     };
