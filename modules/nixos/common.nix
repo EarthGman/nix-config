2d4fa8a34c14dd3972e3dd50e045a1279ef91e74
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-01-31 22:39:13 -0600

    added tree command

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -21,9 +21,11 @@
       zip # archiver
       unzip # archive unzipper
       lshw
+      tree # command to list files as a tree
       etcher # ISO/USB flasher
       radeontop # another htop like monitoring tool for AMD GPUs only
       nvtop # an htop like monitoring tool for GPUs
+      powertop # power consumption monitoring tool
       sysz
       ncdu
       jq
