e236e825d7f21ac24386bc1f339f0c0f1fa48fac
EarthGman <EarthGman@protonmail.com>
2024-02-04 14:59:09 -0600

    multiple desktops

---
+++ modules/nixos/desktops.nix
@@ -0,0 +1,52 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services = {
+    xserver = {
+      desktopManager.gnome.enable = lib.mkDefault true;
+      displayManager.gdm.enable = lib.mkDefault true;
+
+      enable = true;
+
+      layout = "us";
+    };
+  };
+
+  specialisation = {
+    gnome.configuration = {
+      services.xserver = {
+        desktopManager.gnome.enable = true;
+        displayManager.gdm.enable = true;
+      };
+      system.nixos.tags = [ "gnome" ];
+    };
+    pantheon.configuration = {
+      services.xserver = {
+        # Pantheon conflicts with gnome
+        desktopManager = {
+          gnome.enable = false;
+          pantheon.enable = true;
+        };
+        # Pantheon requires lightdm
+        displayManager = {
+          gdm.enable = false;
+          lightdm.enable = true;
+        };
+      };
+      system.nixos.tags = [ "pantheon" ];
+    };
+    xfce.configuration = {
+      networking.networkmanager.enable = true;
+      services.xserver = {
+        desktopManager = {
+          gnome.enable = false;
+          xfce.enable = true;
+        };
+        displayManager = {
+          gdm.enable = false;
+        };
+      };
+      system.nixos.tags = [ "gnome" ];
+    };
+  };
+}
