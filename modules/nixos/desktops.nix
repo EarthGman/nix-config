82dfcdd5d4241c623d3dd00e78638f9dbccdc106
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-02-25 17:50:04 -0600

    removed alot of useless stuff

--- modules/nixos/desktops.nix
+++ modules/nixos/desktops.nix
@@ -2,6 +2,10 @@
 {
   # resolve for gnome and cinnamon confliction.
   environment.sessionVariables.NIX_GSETTINGS_OVERRIDES_DIR = lib.mkForce "/nix/store/bmn25wcr6rp682bkyvjsj7yddlln4ldv-cinnamon-gsettings-overrides/share/gsettings-schemas/nixos-gsettings-overrides/glib-2.0/schemas";
+  # resolve conflict for plasma and gnome
+  programs.ssh.askPassword = lib.mkForce "/nix/store/0dsjcbp33ibm4zkbhm99d3fxslnaj28v-seahorse-43.0/libexec/seahorse/ssh-askpass";
+  # force disable geary for gnome and cinnamon
+  programs.geary.enable = false;
 
   services.xserver = {
     enable = true;
@@ -14,6 +18,7 @@
     };
   };
 
+  programs.dconf.enable = true;
   # stuff for the desktops
   qt = {
     enable = true;
@@ -21,20 +26,20 @@
     style = "adwaita-dark";
   };
 
-  programs.dconf.enable = true;
-  # resolve conflict for plasma and gnome
-  programs.ssh.askPassword = lib.mkForce "/nix/store/0dsjcbp33ibm4zkbhm99d3fxslnaj28v-seahorse-43.0/libexec/seahorse/ssh-askpass";
-
   environment.gnome.excludePackages = (with pkgs; [
+    weather
     gnome-photos
     gnome-tour
     gedit
+    hexchat
+    loupe
   ]) ++ (with pkgs.gnome; [
     cheese
     gnome-music
     epiphany
-    # geary
+    geary
     gnome-characters
+    nautilus
     tali
     iagno
     hitori
@@ -44,13 +49,29 @@
     gnome-initial-setup
   ]);
 
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    ark
+    elisa
+    gwenview
+    konsole
+    kwalletmanager
+    okular
+    pix
+    spectacle
+  ];
+
+  environment.cinnamon.excludePackages = (with pkgs; [
+    hexchat
+    weather
+    xed-editor
+  ]) ++ (with pkgs.cinnamon; [
+    nemo
+    pix
+  ]);
+
   environment.systemPackages = with pkgs; [
     gnome.gnome-tweaks
     gnome.gnome-system-monitor
     #gnomeExtensions.custom-vpn-toggler
-    evince
-    libsForQt5.kalk # calculator
-    libsForQt5.kpmcore # library for partition manager
-    partition-manager # KDE partition manager
   ];
 }
