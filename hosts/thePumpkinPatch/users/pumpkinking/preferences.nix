84eb11fc9679a33545e3a62861218798c01873ae
Thunderbean290 <156272091+Thunderbean290@users.noreply.github.com>
2024-08-07 14:18:30 -0500

    fixed spacing in preferences

--- hosts/thePumpkinPatch/users/pumpkinking/preferences.nix
+++ hosts/thePumpkinPatch/users/pumpkinking/preferences.nix
@@ -1,6 +1,7 @@
 { outputs, ... }:
 {
   # extra config for home-manager. anything here will not apply to any user on any system other than this one
+
   #firefox
   firefox.theme.name = "shyfox";
   firefox.theme.config.wallpaper = outputs.wallpapers.two-hallow-knights;
