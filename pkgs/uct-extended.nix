382d80e4c4c480b550faf1f899cca0d6d9ad4768
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-08-15 21:36:01 -0500

    fixed version mistake in uct-extended

--- pkgs/uct-extended.nix
+++ pkgs/uct-extended.nix
@@ -31,11 +31,11 @@ let
     }
   );
 in
-buildFirefoxXpiAddon {
+buildFirefoxXpiAddon rec {
   pname = "userchrome-toggle-extended";
   version = "2.0";
   addonId = "userchrome-toggle-extended@n2ezr.ru";
-  url = "https://addons.mozilla.org/firefox/downloads/file/4331605/userchrome_toggle_extended-2.0.xpi";
+  url = "https://addons.mozilla.org/firefox/downloads/file/4331605/userchrome_toggle_extended-${version}.xpi";
   sha256 = "1b5r2pbxyj1wcad1myiah863r70d0gl5nnl2l7zf37njdy2avq02";
 
   meta = with lib; {
