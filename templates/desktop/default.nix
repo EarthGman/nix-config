54c77880561e11cfa6be0bc8ef5132dc6e901895
EarthGman <EarthGman@protonmail.com>
2024-09-04 18:52:05 -0500

    forgot this

--- templates/desktop/default.nix
+++ templates/desktop/default.nix
@@ -18,6 +18,15 @@ in
     printing.enable = mkDefault true;
     ifuse.enable = mkDefault true;
   };
+  # decorate shell for root by default ~2.5GB of bloat
+  programs = {
+    zsh = {
+      enableCompletion = mkDefault true;
+      syntaxHighlighting.enable = mkDefault true;
+    };
+    starship.enable = mkDefault true;
+  };
+
   # if you have dolphin emu installed GC controllers will not have correct permissions unless set
   udev.packages = [ pkgs.dolphinEmu ];
 }
