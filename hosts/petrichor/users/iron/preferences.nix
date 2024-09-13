54eecab7e9260c05ac3e3b2af20ea2d6a1c727d5
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-09-12 19:30:32 -0500

    more apps for iron

--- hosts/petrichor/users/iron/preferences.nix
+++ hosts/petrichor/users/iron/preferences.nix
@@ -9,20 +9,33 @@ in
 
   custom = {
     #firefox
+    preferredEditor = "codium";
+    firefox.enable = true;
     firefox.theme.name = "shyfox";
     firefox.theme.config.wallpaper = fetchurl wallpapers.get-mooned;
 
+    bottles.enable = true;
     lutris.enable = true;
     prismlauncher.enable = true;
     discord.enable = true;
     gimp.enable = true;
     yazi.enable = true;
 
+    museeks.enable = true;
+    clipgrab.enable = true;
+    obs.enable = true;
+    openshot.enable = true;
+
+    audacity.enable = true;
+    pwvucontrol.enable = true;
+    libreoffice.enable = true;
+    totem.enable = true;
     evince.enable = true;
     fastfetch.enable = true;
     gnome-calculator.enable = true;
     gnome-system-monitor.enable = true;
     gthumb.enable = true;
     switcheroo.enable = true;
+    nautilus.enable = true;
   };
 }
