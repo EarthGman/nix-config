b2ba0e2a76d47417068231723f39ccb398c8699c
EarthGman <EarthGman@protonmail.com>
2024-10-15 08:29:44 -0500

    extra screenshot key for iron

--- hosts/petrichor/users/iron/preferences.nix
+++ hosts/petrichor/users/iron/preferences.nix
@@ -1,4 +1,4 @@
-{ self, ... }:
+{ self, pkgs, lib, config, ... }:
 let
   theme = self + /profiles/home-manager/desktop-themes/vibrant-cool.nix;
 in
@@ -39,4 +39,8 @@ in
     switcheroo.enable = true;
     nautilus.enable = true;
   };
+  # no printscreen, use mainMod + f1 instead
+  wayland.windowManager.hyprland.extraConfig = ''
+    bind=${config.wayland.windowManager.hyprland.mainMod}, F1, exec, ${lib.getExe pkgs.grim} -g \"$(${lib.getExe pkgs.slurp})\" - | ${pkgs.wl-clipboard}/bin/wl-copy && ${pkgs.wl-clipboard}/bin/wl-paste > ${config.home.homeDirectory}/Pictures/Screenshots/Screenshot-$(date +%F_%T).png
+  '';
 }
