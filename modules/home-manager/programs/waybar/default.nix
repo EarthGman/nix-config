adb0113c166c55aea02f8e6e40ac831d20d504d3
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-10-15 08:07:12 -0500

    only write settings-menu.xml if waybar is enabled

--- modules/home-manager/programs/waybar/default.nix
+++ modules/home-manager/programs/waybar/default.nix
@@ -32,41 +32,44 @@ in
     ]);
     # settings menu
     xdg.configFile = {
-      "waybar/settings-menu.xml".text = ''
-                <?xml version="1.0" encoding="UTF-8"?>
-          <interface>
-            <object class="GtkMenu" id="menu">
-            <child>
-              <object class="GtkMenuItem" id="wallpaper-switcher">
-          			<property name="label">󰸉 Wallpapers</property>
-              </object>
-          	</child>
-            <child>
-              <object class="GtkMenuItem" id="theme-switcher">
-          			<property name="label"> Themes</property>
-              </object>
-          	</child>
-          	<child>
-              <object class="GtkMenuItem" id="lockscreen">
-          			<property name="label"> Lock Screen</property>
-              </object>
-          	</child>
-            <child>
-            <object class="GtkMenuItem" id="reboot">
-              <property name="label"> Reboot</property>
-            </object>
-            </child>
-            <child>
-              <object class="GtkSeparatorMenuItem" id="delimiter1"/>
-            </child>
-            <child>
-                <object class="GtkMenuItem" id="shutdown">
-              <property name="label"> Shutdown</property>
+      "waybar/settings-menu.xml" = {
+        enable = cfg.enable;
+        text = ''
+            <?xml version="1.0" encoding="UTF-8"?>
+            <interface>
+              <object class="GtkMenu" id="menu">
+              <child>
+                <object class="GtkMenuItem" id="wallpaper-switcher">
+            			<property name="label">󰸉 Wallpapers</property>
                 </object>
-            </child>
-          </object>
-        </interface>
-      '';
+            	</child>
+              <child>
+                <object class="GtkMenuItem" id="theme-switcher">
+            			<property name="label"> Themes</property>
+                </object>
+            	</child>
+            	<child>
+                <object class="GtkMenuItem" id="lockscreen">
+            			<property name="label"> Lock Screen</property>
+                </object>
+            	</child>
+              <child>
+              <object class="GtkMenuItem" id="reboot">
+                <property name="label"> Reboot</property>
+              </object>
+              </child>
+              <child>
+                <object class="GtkSeparatorMenuItem" id="delimiter1"/>
+              </child>
+              <child>
+                  <object class="GtkMenuItem" id="shutdown">
+                <property name="label"> Shutdown</property>
+                  </object>
+              </child>
+            </object>
+          </interface>
+        '';
+      };
     };
   };
 }
