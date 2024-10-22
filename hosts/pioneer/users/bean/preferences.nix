f664a61765ad1179c65f64515c58fa468167fa97
ThunderBean290 <156272091+Thunderbean290@users.noreply.github.com>
2024-10-22 13:35:45 -0500

    enable vesktop on pioneer

--- hosts/pioneer/users/bean/preferences.nix
+++ hosts/pioneer/users/bean/preferences.nix
@@ -9,6 +9,10 @@ in
     theme
   ];
 
+  programs = {
+    discord.enable = true;
+  };
+
   services.polybar.settings = {
     "bar/bottom" = {
       font-0 = "MesloLGS Nerd Font Mono:size = 12;4";
