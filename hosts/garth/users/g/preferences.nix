e54cd03b315c0c624189452d8fa08243f5320bf6
EarthGman <EarthGman@protonmail.com>
2024-10-23 22:06:08 -0500

    fastfetch image randomizer

--- hosts/garth/users/g/preferences.nix
+++ hosts/garth/users/g/preferences.nix
@@ -1,5 +1,6 @@
-{ self, ... }:
+{ self, icons, ... }:
 let
+  inherit (builtins) fetchurl;
   profile = self + /profiles/home-manager/g.nix;
   theme = self + /profiles/home-manager/desktop-themes/faraway.nix;
 in
@@ -14,6 +15,15 @@ in
     lutris.enable = true;
     moonlight.enable = true;
     gnome-clocks.enable = true;
+
+    fastfetch.imageRandomizer = {
+      enable = true;
+      images = [
+        (fetchurl icons.oops)
+        (fetchurl icons.nixos-clean)
+        (fetchurl icons.kaori)
+      ];
+    };
   };
 }
 
--- modules/home-manager/programs/fastfetch/default.nix
+++ modules/home-manager/programs/fastfetch/default.nix
@@ -1,21 +1,48 @@
 { pkgs, lib, config, ... }:
 let
+  inherit (lib) mkEnableOption mkOption mkIf types getExe;
   cfg = config.programs.fastfetch;
+  image-randomizer = pkgs.writeScript "fastfetch-image-randomizer.sh" ''
+    #!/usr/bin/env bash
+
+    images=(${toString (lib.concatStringsSep " " cfg.imageRandomizer.images)})
+    image_count=''${#images[@]}
+    random_index=$((RANDOM % image_count))
+    selected_image=''${images[$random_index]}
+
+    fastfetch -l "$selected_image"
+  '';
 in
 {
   options.programs.fastfetch = {
-    image = lib.mkOption {
+    image = mkOption {
       description = "image used for fastfetch";
-      type = lib.types.str;
+      type = types.str;
       default = "nixos";
     };
+    imageRandomizer.enable = mkEnableOption ''
+      whether to enable randomized fastfetch image display
+      if unused, programs.fastfetch.image will be used instead
+    '';
+    imageRandomizer.images = mkOption {
+      description = "list of images that will be passed to the fastfetch randomizer";
+      type = types.listOf types.str;
+      default = [ cfg.image ]; # just in case its enabled but no images are provided
+    };
   };
-  config = lib.mkIf cfg.enable {
+
+  config = mkIf cfg.enable {
     programs.fastfetch = {
       settings = import ./settings.nix { inherit config; };
     };
-    programs.zsh.shellAliases = {
-      ff = "${lib.getExe pkgs.fastfetch}";
-    };
+    # direct the alias to the randomizer script
+    programs.zsh.shellAliases =
+      if (cfg.imageRandomizer.enable)
+      then
+        { ff = "${image-randomizer}"; }
+      else
+        { ff = "${getExe pkgs.fastfetch}  "; };
   };
 }
+
+
