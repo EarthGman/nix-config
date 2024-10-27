4099e778c578fe4444713b3c8541d0fc60b77007
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-10-27 18:20:15 -0500

    whoops

--- profiles/nixos/server/disko.nix
+++ profiles/nixos/server/disko.nix
@@ -20,7 +20,7 @@
               size = "100%";
               content = {
                 type = "filesystem";
-                format = "btrfs";
+                format = "ext4";
                 mountpoint = "/";
               };
             };
