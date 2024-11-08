aa36414e9b02df2ac4cdf1bd8aae130d6b4fc05d
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-11-07 19:58:12 -0600

    tweak omori calendar service

--- modules/home-manager/services/omori-calendar/default.nix
+++ modules/home-manager/services/omori-calendar/default.nix
@@ -1,53 +1,8 @@
-{ pkgs, lib, config, wallpapers, ... }:
+{ pkgs, lib, myLib, config, desktop, wallpapers, ... }:
 let
-  inherit (pkgs) writeScript;
+  inherit (lib) getExe;
   inherit (builtins) fetchurl;
-  script = writeScript "set-wallpaper-by-month.sh" ''
-    #!${pkgs.bash}/bin/bash
-   
-    January="${fetchurl wallpapers.omori-january}"
-    Feburary="${fetchurl wallpapers.omori-feburary}"
-    March="${fetchurl wallpapers.omori-march}"
-    April="${fetchurl wallpapers.omori-april}"
-    May="${fetchurl wallpapers.omori-may}"
-    June="${fetchurl wallpapers.omori-june}"
-    July="${fetchurl wallpapers.omori-july}"
-    August="${fetchurl wallpapers.omori-august}"
-    September="${fetchurl wallpapers.omori-september}"
-    October="${fetchurl wallpapers.omori-october}"
-    November="${fetchurl wallpapers.omori-november}"
-    December="${fetchurl wallpapers.omori-december}"
-  
-    MONTH=$(/run/current-system/sw/bin/date +"%B")
-    
-    case "$XDG_CURRENT_DESKTOP" in
-      "Hyprland")
-        for i in {1..5}; do
-        hyprctl clients > /dev/null 2>&1
-        if [ $? -eq 0 ]; then
-          break
-        fi
-        sleep 1
-        done
-        if ! hyprctl clients > /dev/null 2>&1; then
-          exit 1
-        fi
-
-        CURRENT_CHECK=$(hyprctl hypaper listloaded | grep -i "omori-''${MONTH}")
-        if [ -n "$CURRENT_CHECK" ]; then
-          exit 0
-        fi
-
-        hyprctl hyprpaper preload "''${!MONTH}"
-        hyprctl hyprpaper wallpaper ",''${!MONTH}"
-        hyprctl hyprpaper unload
-        exit 0
-        ;;
-      *)
-        ${lib.getExe pkgs.feh} --bg-scale "''${!MONTH}"
-        ;;
-    esac
-  '';
+  script = import ./script.nix { inherit pkgs fetchurl wallpapers getExe; };
 in
 {
   options.services.omori-calendar-project.enable = lib.mkEnableOption ''
@@ -106,5 +61,15 @@ in
         notification = false;
       }
     ];
+
+    # if gnome is enabled give a warning
+    warnings =
+      let
+        desktops = myLib.splitToList desktop;
+        gnome = builtins.elem "gnome" desktops;
+      in
+      if gnome then
+        [ "The Omori Calendar Project service does not work on gnome. You are seeing this because you have both gnome and the service enabled." ]
+      else [ ];
   };
 }
