3bcf9bb08842bd14ddfc926c7ed95ff7d055681b
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-11-26 14:24:39 -0600

    wireguard restart hook

--- profiles/nixos/wg0.nix
+++ profiles/nixos/wg0.nix
@@ -1,8 +1,21 @@
 # EarthGman's client wireguard setup.
-{ config, ... }:
+{ pkgs, config, ... }:
 {
   sops.secrets.wg0_conf.path = "/etc/wireguard/wg0.conf";
   networking = {
+    networkmanager.dispatcherScripts = [{
+      source = pkgs.writeScript "wg0-refresh-hook.sh" ''
+        INTERFACE=$1
+        STATE=$2
+
+        if [ "$STATE" != "up" ] || [ "$INTERFACE" = "wg0" ]; then
+          exit 0
+        fi
+
+        systemctl restart wg-quick-wg0
+      '';
+      type = "basic";
+    }];
     # route all traffic destined for a 10.10.x.x address through the wg0 tunnel
     localCommands = ''
       ip route add 10.10.0.0/24 via 10.0.0.1 dev wg0
