e08b13b6ca811ca09defc8d4ad66675883c8e864
EarthGman <EarthGman@protonmail.com>
2024-11-26 21:19:39 -0600

    fix polybar launching on hyprland

--- modules/home-manager/services/polybar/default.nix
+++ modules/home-manager/services/polybar/default.nix
@@ -12,8 +12,11 @@ in
     script = import ./script.nix { inherit pkgs lib; };
     settings = import ./settings.nix { inherit pkgs mkDefault config getExe; };
   };
-  systemd.user.services.polybar.Service.Environment = mkForce [
-    # needs commands from /run/current-system. default did not provide path
-    "PATH=/run/current-system/sw/bin:${pkgs.polybar}/bin:/run/wrappers/bin"
-  ];
+  systemd.user.services.polybar = {
+    Service.Environment = mkForce [
+      # needs commands from /run/current-system. default did not provide path
+      "PATH=/run/current-system/sw/bin:${pkgs.polybar}/bin:/run/wrappers/bin"
+    ];
+    Install.WantedBy = mkForce [ ]; # prevents polybar from launching without a startup script in the TWM configuration
+  };
 }
