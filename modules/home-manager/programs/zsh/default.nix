59caaf8713b6eb12fa5ec57912b432ad0c3ffb46
EarthGman <117403037+EarthGman@users.noreply.github.com>
2024-12-10 00:09:09 -0600

    zsh update

--- modules/home-manager/programs/zsh/default.nix
+++ modules/home-manager/programs/zsh/default.nix
@@ -4,22 +4,25 @@ let
   cfg = config.programs.zsh;
 in
 {
-  config = {
-    programs.zsh = mkIf cfg.enable {
-      shellAliases = {
-        l = "${getExe pkgs.eza} -al --icons";
-        ls = "${getExe pkgs.eza} --icons";
-        lg = "${getExe pkgs.lazygit}";
-        edit-config = "cd ~/src/nix-config && $EDITOR .";
-        edit-preferences = "cd ~/src/nix-config/hosts/${hostName}/users/${config.home.username} && $EDITOR preferences.nix";
-      };
-      initExtra = ''
-        export EDITOR=${config.custom.editor}
-        export PATH=$(realpath ~/bin):$PATH
-      '';
+  programs.zsh = mkIf cfg.enable {
+    autosuggestion.enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    autocd = true;
+    shellAliases = {
+      l = "${getExe pkgs.eza} -al --icons";
+      ls = "${getExe pkgs.eza} --icons";
+      lg = "${getExe pkgs.lazygit}";
+      edit-config = "cd ~/src/nix-config && $EDITOR .";
+      edit-preferences = "cd ~/src/nix-config/hosts/${hostName}/users/${config.home.username} && $EDITOR preferences.nix";
+      man = "${getExe pkgs.bat-extras.batman}";
     };
-    # auto create a .zshrc file with just a comment if no home configuration is specified
-    home.file.".zshrc".text = mkIf (!(cfg.enable)) "#";
+    initExtra = ''
+      setopt interactivecomments
+      compdef batman=man
+      export EDITOR=${config.custom.editor}
+      export PATH=$(realpath ~/bin):$PATH
+    '';
   };
 }
     
