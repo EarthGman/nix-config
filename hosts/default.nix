c1ef301bb37a81439e9462c1b0bc6292db7145dc
EarthGman <EarthGman@protonmail.com>
2024-12-17 02:46:55 -0600

    readd i3/sway on garth

--- hosts/default.nix
+++ hosts/default.nix
@@ -20,7 +20,7 @@ in
     cpu = "intel";
     gpu = "intel-igpu";
     users = [ "g" ];
-    desktop = "gnome";
+    desktop = "i3,sway";
     platform = "x86_64-linux";
     stateVersion = "24.05";
     configDir = ./garth;
