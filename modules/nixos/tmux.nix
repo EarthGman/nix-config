d7a344217a9755244e550ee6fcbcb737988fe778
EarthGman <EarthGman@protonmail.com>
2025-02-25 22:50:04 -0600

    update tmux

--- modules/nixos/tmux.nix
+++ modules/nixos/tmux.nix
@@ -1,4 +1,4 @@
-{ lib, config, ... }:
+{ pkgs, lib, config, ... }:
 let
   inherit (lib) mkIf mkDefault mkEnableOption;
   cfg = config.modules.tmux;
@@ -9,10 +9,23 @@ in
   config = mkIf (cfg.enable) {
     programs.tmux = {
       enable = true;
+      shortcut = mkDefault "space";
       keyMode = mkDefault "vi";
       clock24 = mkDefault true; # military time is better
       baseIndex = mkDefault 1; # lua has corrupted me
+      plugins = with pkgs.tmuxPlugins; [
+        catppuccin
+      ];
+      extraConfig = ''
+        bind-key h select-pane -L
+        bind-key j select-pane -D
+        bind-key k select-pane -U
+        bind-key l select-pane -R
+
+        unbind r
+        bind r source-file /etc/tmux.conf
+        set-option -g status-position top
+      '';
     };
   };
-
 }
