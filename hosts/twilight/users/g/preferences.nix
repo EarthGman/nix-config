593ba02cb8ef19de07071f7466b003e700a008cf
EarthGman <EarthGman@protonmail.com>
2025-03-18 11:07:02 -0500

    add temperature module to twilight

--- hosts/twilight/users/g/preferences.nix
+++ hosts/twilight/users/g/preferences.nix
@@ -9,6 +9,14 @@ in
     prismlauncher.package = pkgs.prismlauncher; # dont build newest version locally
     moonlight.enable = true;
     waybar.bottomBar.settings = {
+      modules-left = [
+        "custom/settings-menu"
+        "network"
+        "cpu"
+        "memory"
+        "disk"
+        "temperature"
+      ];
       temperature = {
         hwmon-path = "/sys/class/hwmon/hwmon3/temp1_input";
         format = " {icon} {temperatureC}°C";
