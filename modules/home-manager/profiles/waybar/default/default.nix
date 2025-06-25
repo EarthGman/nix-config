8545854513c1cd53ce91893327ab17161d32b3f6
EarthGman <EarthGman@protonmail.com>
2025-06-25 13:17:36 -0500

    conditionally set applet services for waybar

--- modules/home-manager/profiles/waybar/default/default.nix
+++ modules/home-manager/profiles/waybar/default/default.nix
@@ -1,5 +1,6 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, config, ... }@args:
 let
+  nixosConfig = if args ? nixosConfig then args.nixosConfig else null;
   inherit (lib) mkIf mkDefault mkEnableOption;
   cfg = config.profiles.waybar.default;
   scripts = import ../../../scripts { inherit pkgs lib config; };
@@ -8,9 +9,25 @@ in
   options.profiles.waybar.default.enable = mkEnableOption "default waybar profile";
 
   config = mkIf cfg.enable {
-    home.packages = mkIf config.programs.waybar.enable (with pkgs; [ nerd-fonts.meslo-lg ]);
-    services.network-manager-applet.enable = mkDefault true;
-    services.blueman-applet.enable = mkDefault true;
+    home.packages = mkIf config.programs.waybar.enable (with pkgs.nerd-fonts; [ meslo-lg ]);
+    services.network-manager-applet.enable =
+      if (nixosConfig != null) then
+        if (nixosConfig.networking.networkmanager.enable) then
+          mkDefault true
+        else
+          false
+      else
+        false;
+
+    services.blueman-applet.enable =
+      if (nixosConfig != null) then
+        if (nixosConfig.services.blueman.enable) then
+          mkDefault true
+        else
+          false
+      else
+        false;
+
     stylix.targets.waybar = {
       addCss = mkDefault false;
     };
