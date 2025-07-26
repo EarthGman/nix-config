e3550067f09cb0888efb302361b993078c8f45c7
EarthGman <EarthGman@protonmail.com>
2025-07-26 18:26:44 -0500

    update cosmos theme

--- modules/home-manager/profiles/desktop-themes/cosmos.nix
+++ modules/home-manager/profiles/desktop-themes/cosmos.nix
@@ -21,18 +21,22 @@ in
     services.swww.slideshow = {
       enable = mkDefault true;
       interval = mkDefault 300;
-      images = mkDefault (with wallpapers; [
-        (fetchurl black-hole)
-        (fetchurl pixel-galaxy)
-        (fetchurl space-piano)
-        (fetchurl galaxy)
-        (fetchurl out-of-this-whirl)
-        (fetchurl nebula)
-        (fetchurl nebula-2)
-        (fetchurl blue-marble)
-        (fetchurl blue-marble-2)
-        (fetchurl blue-marble-3)
-      ]);
+      images = mkDefault (
+        with wallpapers;
+        [
+          (fetchurl black-hole)
+          (fetchurl pixel-galaxy)
+          (fetchurl space-piano)
+          (fetchurl space-jelly)
+          (fetchurl galaxy)
+          (fetchurl out-of-this-whirl)
+          (fetchurl nebula)
+          (fetchurl nebula-2)
+          (fetchurl blue-marble)
+          (fetchurl blue-marble-2)
+          (fetchurl blue-marble-3)
+        ]
+      );
     };
   };
 }
