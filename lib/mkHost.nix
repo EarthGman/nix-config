9744bc54cb3517b464a839e02fe852c41aa30dd2
EarthGman <EarthGman@protonmail.com>
2025-08-24 11:03:25 -0500

    drop lib.mkIf

--- lib/mkHost.nix
+++ lib/mkHost.nix
@@ -40,7 +40,7 @@ lib.nixosSystem {
     [
       {
         # initrd modules for qemu
-        imports = lib.mkIf (vm) [ (inputs.nixpkgs + "/modules/profiles/qemu-guest.nix") ];
+        imports = if (vm) then [ (inputs.nixpkgs + "/modules/profiles/qemu-guest.nix") ] else [ ];
 
         # enable my default module and mixins
         gman = {
