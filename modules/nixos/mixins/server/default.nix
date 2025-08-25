ca11839d98ebba297c92278be4ef918d41f2a4d3
EarthGman <EarthGman@protonmail.com>
2025-08-25 14:28:07 -0500

    add bios checks for server config

--- modules/nixos/mixins/server/default.nix
+++ modules/nixos/mixins/server/default.nix
@@ -8,39 +8,45 @@
 
   options.gman.server.enable = lib.mkEnableOption "gman's configuration for a server";
 
-  config = lib.mkIf config.gman.server.enable {
-    gman = {
-      grub.enable = false;
-      debloat.enable = true;
-    };
-
-    boot.loader = {
-      systemd-boot = {
-        enable = lib.mkDefault true;
-        configurationLimit = lib.mkDefault 2;
+  config = lib.mkIf config.gman.server.enable ([
+    (lib.mkIf (config.meta.bios == "UEFI") {
+      gman.grub.enable = lib.mkForce false;
+      boot.loader = {
+        systemd-boot = {
+          enable = lib.mkDefault true;
+          configurationLimit = lib.mkDefault 2;
+        };
       };
-    };
-
-    # remove emergency mode
-    boot.initrd.systemd.suppressedUnits = lib.mkIf config.systemd.enableEmergencyMode [
-      "emergency.service"
-      "emergency.target"
-    ];
-    systemd = {
-      enableEmergencyMode = false;
-      sleep.extraConfig = ''
-        AllowSuspend=no
-        AllowHibernation=no
-      '';
-    };
-
-    networking.firewall.enable = true;
-
-    services.openssh = {
-      settings = {
-        PasswordAuthentication = lib.mkDefault false;
-        KbdInteractiveAuthentication = lib.mkDefault false;
+    })
+    (lib.mkIf (config.meta.bios == "legacy") {
+      gman.grub.enable = true;
+    })
+    {
+      gman = {
+        debloat.enable = true;
       };
-    };
-  };
+
+      # remove emergency mode
+      boot.initrd.systemd.suppressedUnits = lib.mkIf config.systemd.enableEmergencyMode [
+        "emergency.service"
+        "emergency.target"
+      ];
+      systemd = {
+        enableEmergencyMode = false;
+        sleep.extraConfig = ''
+          AllowSuspend=no
+          AllowHibernation=no
+        '';
+      };
+
+      networking.firewall.enable = true;
+
+      services.openssh = {
+        settings = {
+          PasswordAuthentication = lib.mkDefault false;
+          KbdInteractiveAuthentication = lib.mkDefault false;
+        };
+      };
+    }
+  ]);
 }
