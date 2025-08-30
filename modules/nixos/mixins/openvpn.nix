3a4f780e81e309b34d114c2604b5eefe7ce87b47
EarthGman <EarthGman@protonmail.com>
2025-08-30 10:03:22 -0500

    openvpn tweak

--- modules/nixos/mixins/openvpn.nix
+++ modules/nixos/mixins/openvpn.nix
@@ -10,9 +10,10 @@
   config = lib.mkIf config.gman.openvpn.enable {
     networking.networkmanager.plugins = [ pkgs.networkmanager-openvpn ];
     programs = {
-      openvpn3 = {
-        enable = lib.mkDefault true;
-      };
+      # doesn't build 8-30-2025
+      # openvpn3 = {
+      #   enable = lib.mkDefault true;
+      # };
     };
     environment.systemPackages = [ pkgs.openvpn ];
   };
