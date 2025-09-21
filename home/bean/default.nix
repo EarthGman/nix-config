fd11a39f738d2dcf12c215ba3ab3d66f3505da9a
ThunderBean290 <156272091+Thunderbean290@users.noreply.github.com>
2025-09-20 23:04:42 -0500

    Add signing

--- home/bean/default.nix
+++ home/bean/default.nix
@@ -11,6 +11,13 @@ in
     git = {
       userName = "ThunderBean290";
       userEmail = "156272091+Thunderbean290@users.noreply.github.com";
+      signing = {
+        key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbonyjuIdQxAJ788rfATPHOpc7KLTB9Z64z9r/V7a9N";
+        signByDefault = true;
+      };
+      extraConfig = {
+        gpg.format = "ssh";
+      };
     };
     neovim-custom = {
       enable = true;
