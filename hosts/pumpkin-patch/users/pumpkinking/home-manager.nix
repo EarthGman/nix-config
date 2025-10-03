f2893e8645c376dfb6f734748052faf3c2aa5b1a
EarthGman <EarthGman@protonmail.com>
2025-10-02 21:38:01 -0500

    increase refresh rate for pumpkinking's monitor

--- hosts/pumpkin-patch/users/pumpkinking/home-manager.nix
+++ hosts/pumpkin-patch/users/pumpkinking/home-manager.nix
@@ -7,4 +7,20 @@
       terminal = 20;
     };
   };
+  services.kanshi = {
+    enable = true;
+    settings = [
+      # https://gitlab.freedesktop.org/xorg/xserver/-/issues/899
+      {
+        profile.name = "main";
+        profile.outputs = [
+          {
+            criteria = "DP-3";
+            position = "0,0";
+            mode = "2560x1440@164.96Hz";
+          }
+        ];
+      }
+    ];
+  };
 }
